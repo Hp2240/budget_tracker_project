92b574593d422c7b385d2b345793c18c1b64ed94
Hyeree Park <hp2240@nyu.edu>
2022-09-06 12:01:14 -0400

    added middleware and AppRouter

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const PORT = process.env.PORT || 3001
 
 const app = express()
-// const AppRouter = require('./routes/AppRouter')
+const AppRouter = require('./routes/AppRouter')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-// app.use('/api', AppRouter)
+app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server started on Port: ${PORT}`))
