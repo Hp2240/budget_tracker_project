d3bb107ad14c43cedb0473bd13583b31a99fb84d
Hyeree Park <hp2240@nyu.edu>
2022-09-08 00:56:07 -0400

    set up signin

--- routes/AccountRouter.js
+++ routes/AccountRouter.js
@@ -1,9 +1,15 @@
 const Router = require('express').Router()
 const controller = require('../controllers/AccountController')
+const middleware = require('../middleware')
 
 Router.get('/all', controller.findAllAccounts)
 Router.get('/:userId', controller.findAccountsByUser)
-Router.post('/', controller.createAccount)
+Router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.createAccount
+)
 Router.put('/:accountId', controller.updateAccount)
 Router.delete('/:accountId', controller.deleteAccount)
 
